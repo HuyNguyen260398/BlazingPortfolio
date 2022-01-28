a4d05eae5f0901a94271a2a11d6e96c1946ecd0a
Huy Nguyen <huynguyen260398@gmail.com>
2022-01-28 13:19:40 +0700

    Added BaseRepo for Blazor WASM.

---
+++ src/WASM/Interfaces/IBaseRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace WASM.Interfaces;
+
+public interface IBaseRepo<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync(string url);
+    Task<T> GetByIdAsync(string url, int id);
+    Task<bool> CreateAsync(string url, T obj);
+    Task<bool> UpdateAsync(string url, T obj);
+    Task<bool> DeleteAsync(string url, int id);
+}
---
+++ src/WASM/Static/ApiEndPoints.cs
@@ -0,0 +1,11 @@
+﻿namespace WASM.Static;
+
+internal static class ApiEndPoints
+{
+#if DEBUG
+    internal const string ServerBaseUrl = "https://localhost:7054";
+#else
+    internal const string ServerBaseUrl = "";
+#endif
+    internal readonly static string UserEndpoint = $"{ServerBaseUrl}/api/categories";
+}
