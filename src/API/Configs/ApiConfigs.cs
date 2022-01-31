9ea43ed6554a3127d1c507ebcbe29516dd21e80c
Huy Nguyen <huynguyen260398@gmail.com>
2022-01-31 10:30:16 +0700

    Added api tags in Swagger UI.

--- src/API/Configs/ApiConfigs.cs
+++ src/API/Configs/ApiConfigs.cs
@@ -25,33 +25,33 @@ public static class ApiConfigs
     public static void ConfigureApi(this WebApplication app)
     {
         // User
-        app.MapGet("/api/users", async () => await _userController.GetUser());
-        app.MapPut("/api/users", async (UserDto userDto) => await _userController.Update(userDto));
+        app.MapGet("/api/users", async () => await _userController.GetUser()).WithTags("User");
+        app.MapPut("/api/users", async (UserDto userDto) => await _userController.Update(userDto)).WithTags("User");
 
         // Service
-        app.MapGet("/api/services", async () => await _serviceController.GetAll());
-        app.MapGet("/api/services/{id}", async (int id) => await _serviceController.GetById(id));
-        app.MapPost("/api/services", async (ServiceDto serviceDto) => await _serviceController.Create(serviceDto));
-        app.MapPut("/api/services", async (ServiceDto serviceDto) => await _serviceController.Update(serviceDto));
-        app.MapDelete("/api/services/{id}", async (int id) => await _serviceController.Delete(id));
+        app.MapGet("/api/services", async () => await _serviceController.GetAll()).WithTags("Service");
+        app.MapGet("/api/services/{id}", async (int id) => await _serviceController.GetById(id)).WithTags("Service");
+        app.MapPost("/api/services", async (ServiceDto serviceDto) => await _serviceController.Create(serviceDto)).WithTags("Service");
+        app.MapPut("/api/services", async (ServiceDto serviceDto) => await _serviceController.Update(serviceDto)).WithTags("Service");
+        app.MapDelete("/api/services/{id}", async (int id) => await _serviceController.Delete(id)).WithTags("Service");
 
         // Image
-        app.MapGet("/api/images", async () => await _imageController.GetAll());
-        app.MapPost("/api/images", async (ImageDto imageDto) => await _imageController.UploadImage(imageDto));
-        app.MapDelete("/api/images/{guid}", async (string guid) => await _imageController.RemoveImage(guid));
+        app.MapGet("/api/images", async () => await _imageController.GetAll()).WithTags("Image");
+        app.MapPost("/api/images", async (ImageDto imageDto) => await _imageController.UploadImage(imageDto)).WithTags("Image");
+        app.MapDelete("/api/images/{guid}", async (string guid) => await _imageController.RemoveImage(guid)).WithTags("Image");
 
         // Archievement
-        app.MapGet("/api/archievements", async () => await _archievementController.GetAll());
-        app.MapGet("/api/archievements/{id}", async (int id) => await _archievementController.GetById(id));
-        app.MapPost("/api/archievements", async (ArchievementDto archievementDto) => await _archievementController.Create(archievementDto));
-        app.MapPut("/api/archievements", async (ArchievementDto archievementDto) => await _archievementController.Update(archievementDto));
-        app.MapDelete("/api/archievements/{id}", async (int id) => await _archievementController.Delete(id));
+        app.MapGet("/api/archievements", async () => await _archievementController.GetAll()).WithTags("Archievement");
+        app.MapGet("/api/archievements/{id}", async (int id) => await _archievementController.GetById(id)).WithTags("Archievement");
+        app.MapPost("/api/archievements", async (ArchievementDto archievementDto) => await _archievementController.Create(archievementDto)).WithTags("Archievement");
+        app.MapPut("/api/archievements", async (ArchievementDto archievementDto) => await _archievementController.Update(archievementDto)).WithTags("Archievement");
+        app.MapDelete("/api/archievements/{id}", async (int id) => await _archievementController.Delete(id)).WithTags("Archievement");
 
         // Post
-        app.MapGet("/api/posts", async () => await _postController.GetAll());
-        app.MapGet("/api/posts/{id}", async (int id) => await _postController.GetById(id));
-        app.MapPost("/api/posts", async (PostDto postDto) => await _postController.Create(postDto));
-        app.MapPut("/api/posts", async (PostDto postDto) => await _postController.Update(postDto));
-        app.MapDelete("/api/posts/{id}", async (int id) => await _postController.Delete(id));
+        app.MapGet("/api/posts", async () => await _postController.GetAll()).WithTags("Post");
+        app.MapGet("/api/posts/{id}", async (int id) => await _postController.GetById(id)).WithTags("Post");
+        app.MapPost("/api/posts", async (PostDto postDto) => await _postController.Create(postDto)).WithTags("Post");
+        app.MapPut("/api/posts", async (PostDto postDto) => await _postController.Update(postDto)).WithTags("Post");
+        app.MapDelete("/api/posts/{id}", async (int id) => await _postController.Delete(id)).WithTags("Post");
     }
 }
