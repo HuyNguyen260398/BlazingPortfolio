b4d2108e73da0ee1aed59c7e703331a3ee1e30f5
Huy Nguyen <huynguyen260398@gmail.com>
2022-02-21 08:56:15 +0700

    Added Archievement pages.

--- src/Application/Dtos/ArchievementDto.cs
+++ src/Application/Dtos/ArchievementDto.cs
@@ -3,6 +3,10 @@
 public class ArchievementDto
 {
     public int ArchievementId { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required]
     public int Count { get; set; }
 }
--- src/Application/Dtos/ServiceDto.cs
+++ src/Application/Dtos/ServiceDto.cs
@@ -3,7 +3,12 @@
 public class ServiceDto
 {
     public int ServiceId { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required]
     public string Description { get; set; }
+
     public string DisplayIcon { get; set; }
 }
