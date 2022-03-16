ae9ce1de301755f59182bfa05d3b53abb04783e7
Huy Nguyen <huynguyen260398@gmail.com>
2022-03-16 21:39:12 +0700

    Update Swagger to be used in production.

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -158,11 +158,14 @@ app.MapGet("/AuthorizedResource", [Authorize] () => "Action Succeeded");
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
